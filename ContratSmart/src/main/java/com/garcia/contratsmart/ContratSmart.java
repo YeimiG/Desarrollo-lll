545057307cdafcbb4c9240c37913931f89474deb
Yeimi <161745389+YeimiG@users.noreply.github.com>
2026-08-21 19:26:52 -0600

    pull
    pull

---
+++ ContratSmart/src/main/java/com/garcia/contratsmart/ContratSmart.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.garcia.contratsmart;
+
+/**
+ *
+ * @author YEIMI
+ */
+public class ContratSmart {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
